b6b1be1389c91a97a784c0f507a71f0b0835e466
Nandini Agarwal <nia78831@gmail.com>
2021-03-05 12:28:29 +0530

    nandini7637 added day4 solution

---
+++ Day 4/CodeTeam/nandini7637.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class nandini7637{
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter character");
+        char a  = sc.next().charAt(0);
+        int n = a;
+        System.out.println(n);
+}
+}
