e710850bafc78a0a7e7e81820a8dc60674e31e31
MelasMalephar <namitsharma2109@gmail.com>
2021-03-05 15:20:09 +0530

    MelasMalephar Day 1 Contribution

---
+++ Day 1/MelasMalephar.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+ int n;
+ clrscr();
+ cout<<"Enter the number to be checked :";
+ cin>>n;
+ if( n%2 == 0)
+ cout<<n<<" is a even number";
+ else
+ cout<<n<<" is a odd number";
+ getch();
+
+}
