47670503fa6cdfbe66b6f30915ccbc653517cc84
Ankita <66182098+iamAnki@users.noreply.github.com>
2021-03-06 13:34:31 +0530

    Add Day 5 program solution

---
+++ Day5/iamAnki.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,m;
+cout<<"Enter starting range ";
+cin>>n;
+cout<<"Enter last range";
+cin>>m;
+cout<<"Natural number between entered range";
+for(int i=n;i<m;i++)
+{
+cout<<i<<endl;
+}
+return 0;
+}
